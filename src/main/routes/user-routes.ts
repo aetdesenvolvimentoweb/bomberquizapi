fe459a97fa5b8fcb6e32e132268819f2cde95f4c
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-03-22 09:58:14 -0300

    refactor: add user list to user routes

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from "fastify";
 
 import { fastifyRouteAdapter } from "@/infra/adapters";
-import { makeUserCreateController } from "@/infra/factories/controllers";
+import {
+  makeUserCreateController,
+  makeUserListController,
+} from "@/infra/factories/controllers";
 
 export const setupUserRoutes = (app: FastifyInstance): void => {
   app.post("/api/users", fastifyRouteAdapter(makeUserCreateController()));
+  app.get("/api/users", fastifyRouteAdapter(makeUserListController()));
 };
